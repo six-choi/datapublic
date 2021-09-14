057d6658f7acd7b9f981ea189bb3e398b0188cca
six-choi <63387234+six-choi@users.noreply.github.com>
2021-09-14 09:46:45 +0900

    Update routes.rb
    1. 서울디지털사회혁신 아카이브의 소식 부분의 노출되지 않는 포스팅 노출 이슈 (가운데 플렉스 노출안됨)
    2. 노출 이슈가 반복되지 않는 콘텐츠 작성을 위한 관리자 지정 절차를 알고 싶습니다.

--- config/routes.rb
+++ config/routes.rb
@@ -20,3 +20,5 @@ Rails.application.routes.draw do
     resources :assets, only: [:create, :destroy]
   end
 end
+
+#편집자 권한이 없는 가입자가 아카이브에 작성한 글이 표현되지 않는 이슈 대응이 필요합니다. post.68 게시글 이고요. 서울 디지털 사회혁신 센터 아카이브 소식 부분의 가운데 있습니다. 
